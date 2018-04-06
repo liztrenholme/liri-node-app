aa190b8d1d7f069369a7cba30b1b32b33dbab9d8
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-05 23:24:02 -0400

    pseudocoding and defininf functions and variables

--- liri.js
+++ liri.js
@@ -1,26 +1,53 @@
 
 require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require('fs'); //file system
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
 
-const keys = require("./keys.js")
 
 var input = process.argv[2];
-
-if (input === "my-tweets") {
-
+for (i = 4; i < process.argv.length; i++) {
+    userInput += " " + process.argv[i];
 }
 
-if (input === "spotify-this-song" + song) {
 
+var spotify = new Spotify({
+        id: SPOTIFY_ID,
+        secret: SPOTIFY_SECRET
+    });
+
+var task = function(choice, input) {
+    switch (choice) {
+        case "movie-this":
+            getMovie(input);
+            break;
+        case "my-tweets":
+            getTweets(input);
+            break;
+        case "spotify-this-song":
+            getMusic(input);
+            break;
+        case "do-what-it-says":
+            doWhatItSays(input);
+            break;
+    }
 }
-else if (input === "spotify-this-song") {
-	song = "The Sign";
-	
-}
 
-if (input === "movie-this") {
+var getMovie = function(input) {
+	//will search OMDB for movie title and return data to console
+};
 
-}
+var getTweets = function(input) {
+	//will grab last 10 tweets from twitter account and display to console
+};
 
-if (input === "do-what-it-says") {
+var getMusic = function(input) {
+	//will grab song title and artist ansd display info to console
+	//will display "The Sign" by Ace of Base if no song provided
+};
 
-}
+var doWhatItSays = function(input) {
+	//will follow the command given in random.txt
+};
