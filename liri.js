d6833bcad5d4fcdb2e636899f11c3d1ee7269931
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-06 00:33:00 -0400

    added in spotify stuff, but need to debug

--- liri.js
+++ liri.js
@@ -9,14 +9,14 @@ var request = require('request');
 
 var input = process.argv[2];
 for (i = 4; i < process.argv.length; i++) {
-    userInput += " " + process.argv[i];
+    input += " " + process.argv[i];
 }
 
 
-var spotify = new Spotify({
-        id: SPOTIFY_ID,
-        secret: SPOTIFY_SECRET
-    });
+//var spotify = new Spotify({
+  //      id: SPOTIFY_ID,
+    //    secret: SPOTIFY_SECRET
+    //});
 
 var task = function(choice, input) {
     switch (choice) {
@@ -32,6 +32,8 @@ var task = function(choice, input) {
         case "do-what-it-says":
             doWhatItSays(input);
             break;
+        default:
+        	console.log("That didn't work. Try movie-this, my-tweets, spotify-this-song, or do-what-it-says");
     }
 }
 
@@ -43,10 +45,36 @@ var getTweets = function(input) {
 	//will grab last 10 tweets from twitter account and display to console
 };
 
-var getMusic = function(input) {
+
 	//will grab song title and artist ansd display info to console
 	//will display "The Sign" by Ace of Base if no song provided
+var getMusic = function(song) {
+  if (song === undefined) {
+    song = "The Sign";
+  };
+
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
+
+    var songs = data.tracks.items;
+    var data = [];
+
+    for (var i = 0; i < songs.length; i++) {
+      data.push({
+        'artist(s)': songs[i].artists.map(getArtistNames),
+        'song name: ': songs[i].name,
+        'preview song: ': songs[i].preview_url,
+        'album: ': songs[i].album.name,
+      });
+    }
+    console.log(data);
+    writeToLog(data);
+  });
 };
+
 
 var doWhatItSays = function(input) {
 	//will follow the command given in random.txt
