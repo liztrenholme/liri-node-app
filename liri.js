81cf7457b4917f02155b1190b317c6e44352e4de
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-04-05 22:31:27 -0400

    setting up initial files

---
+++ liri.js
@@ -0,0 +1,26 @@
+
+require("dotenv").config();
+
+const keys = require("./keys.js")
+
+var input = process.argv[2];
+
+if (input === "my-tweets") {
+
+}
+
+if (input === "spotify-this-song" + song) {
+
+}
+else if (input === "spotify-this-song") {
+	song = "The Sign";
+	
+}
+
+if (input === "movie-this") {
+
+}
+
+if (input === "do-what-it-says") {
+
+}
